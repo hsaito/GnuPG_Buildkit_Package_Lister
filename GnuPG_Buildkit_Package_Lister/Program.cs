897011bf950f26aef4da99c41d686412d82ae1b4
Hideki Saito <hidekis@gmail.com>
2017-03-16 23:13:19 -0700

    Simple code to grab GnuPG download page

--- GnuPG_Buildkit_Package_Lister/Program.cs
+++ GnuPG_Buildkit_Package_Lister/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace GnuPG_Buildkit_Package_Lister
 {
@@ -6,7 +9,17 @@ namespace GnuPG_Buildkit_Package_Lister
     {
         static void Main(string[] args)
         {
-            
+            GetWeb("https://gnupg.org/download/index.html").Wait();
+        }
+
+        static async Task<string> GetWeb(string location)
+        {
+            var request = WebRequest.Create(location);
+            var response = await request.GetResponseAsync();
+            var stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string data = await reader.ReadToEndAsync();
+            return data;
         }
     }
 }
