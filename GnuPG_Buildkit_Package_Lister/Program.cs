2c61c88292876f2bbfa3ef7fabb500b280fbdc75
Hideki Saito <hideki@hidekisaito.com>
2017-03-19 19:25:17 -0700

    Refactoring -- message separated out to its own file
    Signed-off-by: Hideki Saito <hideki@hidekisaito.com>

--- GnuPG_Buildkit_Package_Lister/Program.cs
+++ GnuPG_Buildkit_Package_Lister/Program.cs
@@ -14,29 +14,8 @@ using log4net.Config;
 
 namespace GnuPG_Buildkit_Package_Lister
 {
-    class Program
+    partial class Program
     {
-        #region Messages
-        // This is suboptimal way of handling message strings.
-        // But the problem is that .NET Core currently do not have built-in support for Resource Compiler...
-
-        /// <summary>
-        /// Messages string for the program.
-        /// </summary>
-        private struct messages
-        {
-            public static string logging_error = "Problem with the logging facility.\nPerhaps missing a config?";
-            public static string debug_mode = "Compiled for debug.";
-            public static string extract_version = "Extracting version informations...";
-            public static string generate_list = "Generating the package list...";
-            public static string get_remote = "Getting the contents from the remote...";
-            public static string program_completed = "Program completed.";
-            public static string program_starting = "Starting program...";
-            public static string url_fetch = "URL to fetch: ";
-        }
-        #endregion Messages
-
-
         private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
         /// <summary>
